0bf5dfe9a95b751d285d022cbcdfecdb329ce5de
PROkrastinator <vladykin.190103@gmail.com>
2021-09-28 00:07:38 +0300

    Base routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './views/about/about.component';
+import { ArticleComponent } from './views/blog/article/article.component';
+import { BlogComponent } from './views/blog/blog.component';
+import { LoginComponent } from './views/login/login.component';
 import { MainPageComponent } from './views/main-page/main-page.component';
+import { MarathonsComponent } from './views/marathons/marathons.component';
+import { RegisterComponent } from './views/register/register.component';
 
 const routes: Routes = [
-  { path: "/", component: MainPageComponent }
+  { path: "", component: MainPageComponent },
+  { path: "about", component: AboutComponent },
+  { path: "marathons", component: MarathonsComponent },
+  { path: "blog", component: BlogComponent },
+  { path: "article/:id", component: ArticleComponent },
+  { path: "login", component: LoginComponent },
+  { path: "register", component: RegisterComponent },
 ];
 
 @NgModule({
