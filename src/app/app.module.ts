72b26bed5aa37902112e783483d6c244ee815168
PROkrastinator <vladykin.190103@gmail.com>
2021-09-27 21:35:54 +0300

    Base pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './views/main-page/main-page.component';
+import { AboutComponent } from './views/about/about.component';
+import { MarathonsComponent } from './views/marathons/marathons.component';
+import { LoginComponent } from './views/login/login.component';
+import { RegisterComponent } from './views/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    AboutComponent,
+    MarathonsComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
---
+++ src/app/views/marathons/marathons.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-marathons',
+  templateUrl: './marathons.component.html',
+  styleUrls: ['./marathons.component.scss']
+})
+export class MarathonsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
