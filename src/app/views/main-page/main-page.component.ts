572e4daf33f995c2ed062d146cb74fa8823634e5
PROkrastinator <vladykin.190103@gmail.com>
2021-10-02 14:10:54 +0300

    Trying header animation

--- src/app/views/main-page/main-page.component.ts
+++ src/app/views/main-page/main-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener, Input, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, HostListener, Input, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-main-page',
@@ -8,13 +8,18 @@ import { Component, HostListener, Input, OnInit, ViewChild } from '@angular/core
 export class MainPageComponent implements OnInit {
   scrollEventsCounter = 0
   isHeaderVisible = false
-  @ViewChild('hero') hero: HTMLElement | null = null;
+  @ViewChild('hero') hero: ElementRef | null = null;
 
   @HostListener('window:scroll', ['$event']) onScrollEvent($event: any){
     if (this.scrollEventsCounter <= 60) {
       this.scrollEventsCounter += 1
     } else if (!this.isHeaderVisible && this.scrollEventsCounter > 60) {
       this.isHeaderVisible = true
+    }
+    if (this.hero) {
+      console.log(this.hero.nativeElement.scrollHeight);
+      
+      console.log(this.hero);
     }
   } 
   
