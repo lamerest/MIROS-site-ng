1807117b64715655cd3e8aa6c85bdd9b6eb4c638
PROkrastinator <vladykin.190103@gmail.com>
2021-10-19 11:35:19 +0300

    Last changes

--- src/app/components/feedback/feedback.component.ts
+++ src/app/components/feedback/feedback.component.ts
@@ -1,17 +1,36 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 import { IMainPage, IReview } from 'src/app/models/pages';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-feedback',
   templateUrl: './feedback.component.html',
   styleUrls: ['./feedback.component.scss']
 })
-export class FeedbackComponent implements OnInit {
+export class FeedbackComponent implements OnInit, OnChanges {
   @Input() content!: IReview
 
   constructor() { }
 
   ngOnInit(): void {
+    this.changeVideosUrls()
   }
 
+  ngOnChanges() {
+    this.changeVideosUrls()
+  }
+
+  changeSlide(amount: number) {
+
+  }
+
+  changeVideosUrls() {
+    for (let video of this.content.videos) {
+      if (!video.url.includes("http")) {
+        video.url = environment.serverUrl + video.url
+      }
+    }
+    console.log(this.content);
+    
+  }
 }
